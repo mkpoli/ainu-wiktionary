61a03f15885a980bc99cc91710bd17054c9b1194
mkpoli <git@mkpo.li>
2025-12-04 02:11:09 +0900

    feat: add page title

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
+  import { m } from '$lib/paraglide/messages';
   import WiktionaryGenerator from '$lib/components/WiktionaryGenerator.svelte';
 </script>
+
+<svelte:head>
+    <title>{m.title()}</title>
+</svelte:head>
 
 <WiktionaryGenerator />
